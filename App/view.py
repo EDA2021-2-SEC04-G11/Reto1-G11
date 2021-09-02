62add7f22c2b7ad86bcc119bdb44f0c704da2c08
Ronald Yesid Diaz Pardo <r.diazp@uniandes.edu.co>
2021-09-01 21:17:02 -0500

    Documentacion de view.py

--- App/view.py
+++ App/view.py
@@ -1,9 +1,38 @@
-﻿#IMPORTS
+﻿﻿"""
+ * Copyright 2020, Departamento de sistemas y Computación, Universidad
+ * de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
+
 import config as cf
 import sys
 import controller
 from DISClib.ADT import list as lt
 assert cf
+
+"""
+La vista se encarga de la interacción con el usuario
+Presenta el menu de opciones y por cada seleccion
+se hace la solicitud al controlador para ejecutar la
+operación solicitada
+"""
 
 def printMenu():
     print("Bienvenido")
@@ -14,17 +43,26 @@ def printMenu():
     print("5- clasificar las obras por la nacionalidad de sus creadores")
     print("6- transportar obras de un departamento")
     print("7- Reglas de transporte")
+
+
 def initCatalog():
     """
     Inicializa el catalogo de libros
     """
     return controller.initCatalog()
+
+
 def loadData(catalog):
     """
     Carga los libros en la estructura de datos
     """
     controller.loadData(catalog)
+
+
 def printLastArtists(catalog):
+    """
+    Imprime los 3 ultimos elementos de artists en el catalogo
+    """
     size = lt.size(catalog['artists'])
     parent = lt.getElement(catalog['artists'],size)
     firstChild = lt.getElement(catalog['artists'],size-1)
@@ -32,7 +70,12 @@ def printLastArtists(catalog):
     print(parent,'\n')
     print(firstChild,'\n')
     print(secondChild,'\n')
+
+
 def printLastArtworks(catalog):
+    """
+    Imprime los 3 ultimos elementos de artworks en el catalogo
+    """
     size = lt.size(catalog['artworks'])
     parent = lt.getElement(catalog['artworks'],size)
     firstChild = lt.getElement(catalog['artworks'],size-1)
@@ -40,7 +83,10 @@ def printLastArtworks(catalog):
     print(parent,'\n')
     print(firstChild,'\n')
     print(secondChild,'\n')
+
+
 catalog = None
+
 """
 Menu principal
 """
